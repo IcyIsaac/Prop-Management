b82ad4077dde3b5f4d5994d4b5285688e62372a5
Isaac Flores <floreisa001@alpine.local>
2018-08-01 11:45:43 -0600

    Added Login inputs

--- src/components/Auth/signinForm.js
+++ src/components/Auth/signinForm.js
@@ -9,9 +9,26 @@ class SigninForm extends Component {
         return (
             <form className='sign-in-form'>
                 <FormTitle className='sign-in-form_title' text='Login'/>
-                <Field className='sign-in-form_email' component={FormInput} name='email' type='email'/>
+                <Field 
+                    className='sign-in-form_email' 
+                    placeholder='Enter Email'  
+                    name='email' 
+                    type='email'
+                    title="Email"
+                    component={FormInput}
+                />
+            
+                <Field 
+                    className='sign-in-form_password' 
+                    placeholder='Enter Password'  
+                    name='password' 
+                    type='password'
+                    title="Password"
+                    component={FormInput}
+                />
+            
             </form>
-        )
+        );
     }
 } 
 
--- src/components/formFields.js
+++ src/components/formFields.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 
    export class FormInput extends Component {
        render() {
-           const { className, title } = this.props;
+           const { className, title, input, type, placeholder } = this.props;
            return (
                 <div className='form-input'>
-               <label>{title}</label>
-                    <input className={`${className} form-input`}/>
+                    <label className='form-input_title'>{title}</label>
+                    <input 
+                        className={`$(className) form-input_input`} // I changed brackets
+                        type={type}
+                        {...input}
+                        placeholder={placeholder}
+                    />
                 </div>
            )
        }
    }
         
 
+
+                        
